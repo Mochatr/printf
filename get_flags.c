ac54f0d34166fa28a381e79e09e5e0fdf3423b93
Vodoo10 <adnane.mouradi1234@gmail.com>
2023-06-27 05:24:39 -0700

    updated

---
+++ get_flags.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int get_flags(const char *format, int *i)
+{
+	int i, j;
+	int pa = 0;
+	const char arrith[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int arrith_sym[] = {AR_MINUS, AR_PLUS, AR_ZERO, AR_HASH, AR_SPACE, 0};
+
+	for (j = *i + 1; format[j] != '\0'; j++)
+	{
+		for (i = 0; arrith[i] != '\0'; i++)
+			if (format[j] == arrith[i])
+			{
+				pa |= arrith_sym[i];
+				break;
+			}
+
+		if (arrith[i] == 0)
+			break;
+	}
+
+	*i = j - 1;
+
+	return (pa);
+}
+
